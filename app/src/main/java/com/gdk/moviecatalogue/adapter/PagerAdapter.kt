766d9524aa8a9b88ba3718c99c25bb153df27422
eko8757 <ekomurdiansyah89@gmail.com>
2019-09-07 15:20:54 +0700

    [ADD] pager adapter [ADD] favorite movie [ADD] favorite tv [ADD] tab layout

--- app/src/main/java/com/gdk/moviecatalogue/adapter/PagerAdapter.kt
+++ app/src/main/java/com/gdk/moviecatalogue/adapter/PagerAdapter.kt
@@ -1,4 +1,24 @@
 package com.gdk.moviecatalogue.adapter
 
-class PagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class PagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
+    private val fragments = ArrayList<Fragment>()
+    private val titles = ArrayList<String>()
+
+    override fun getItem(position: Int): Fragment = fragments[position]
+
+    override fun getCount(): Int = fragments.size
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titles[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragments.add(fragment)
+        titles.add(title)
+    }
 }
--- app/src/main/java/com/gdk/moviecatalogue/ui/fragment/favorites/FavoriteFragment.kt
+++ app/src/main/java/com/gdk/moviecatalogue/ui/fragment/favorites/FavoriteFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.viewpager.widget.ViewPager
 import com.gdk.moviecatalogue.R
-import com.gdk.moviecatalogue.view.FavoriteView
+import com.gdk.moviecatalogue.adapter.PagerAdapter
+import kotlinx.android.synthetic.main.fragment_favorite.*
 
-class FavoriteFragment : Fragment(), FavoriteView {
+class FavoriteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,15 +20,21 @@ class FavoriteFragment : Fragment(), FavoriteView {
         return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
-    override fun makeToast(msg: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewPager(vp_favorite)
+        tab_favorite.setupWithViewPager(vp_favorite)
     }
 
-    override fun showProgress() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun hideProgress() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    private fun setupViewPager(pager: ViewPager) {
+        val adapter = fragmentManager?.let {
+            PagerAdapter(it)
+        }
+        val movie = FavoriteMovieFragment.movieFavoriteInstance()
+        adapter?.addFragment(movie, "Movie")
+        val tv = FavoriteTvFragment.tvFavoriteInstance()
+        adapter?.addFragment(tv, "Tv Show")
+        pager.adapter = adapter
     }
 }
